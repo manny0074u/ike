3fd1ac8849501b3272e11f9115a210a9b95633c2
Emmanuel <aboajahemmauel@gmail.com>
2017-10-23 17:44:06 -0700

    update

---
+++ inc/header.php
@@ -0,0 +1,213 @@
+<!doctype html>
+<html class="no-js" lang="">
+    
+<!-- Mirrored from irsfoundation.com/tf/templates/legacy-preview/legacy/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 20 Oct 2017 19:49:17 GMT -->
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Legacy || Home One</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="shortcut icon" type="image/png" href="assets/images/favicon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+		<!-- all css here -->
+		<!-- bootstrap v3.3.7 css -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<!-- animate css -->
+        <link rel="stylesheet" href="assets/css/animate.css">
+		<!-- owl.carousel.2.0.0-beta.2.4 css -->
+        <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<!-- font-awesome v4.6.3 css -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<!-- magnific-popup.css -->
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+		<!-- slicknav.min.css -->
+        <link rel="stylesheet" href="assets/css/slicknav.min.css">
+		<!-- nivo-slider.css -->
+        <link rel="stylesheet" href="assets/css/flaticon.css">
+		<!-- style css -->
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<!-- responsive css -->
+        <link rel="stylesheet" href="assets/css/responsive.css">
+		<!-- modernizr css -->
+        <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+		
+		<!--start preloader area-->
+        <div class="loader-container circle-pulse-multiple">
+			<div class="loader">
+				<div id="loading-center-absolute">
+                    <div class="object" id="object_four"></div>
+                    <div class="object" id="object_three"></div>
+                    <div class="object" id="object_two"></div>
+                    <div class="object" id="object_one"></div>
+                </div>
+			</div>
+		</div>
+       <!--End preloader area-->
+		
+		<!-- heared area start -->
+		<header class="header-area" id="sticky-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 col-sm-8 col-xs-7">
+						<div class="logo">
+							<a href="index.html">
+								<img src="assets/images/logo.png" alt="" />
+							</a>
+						</div>
+					</div>
+					<div class="col-md-8 hidden-sm hidden-xs">
+						<div class="mainmenu">
+							<ul id="navigation">
+								<li class="active"><a href="index.html">Home <i class="fa fa-angle-down"></i></a>
+									<ul class="submenu">
+										<li class="active"><a href="index.html">Home One</a></li>
+										<li><a href="index2.html">Home Two</a></li>
+										<li><a href="index3.html">Home Three</a></li>
+									</ul>
+								</li>
+								<li><a href="service.html">Service <i class="fa fa-angle-down"></i></a>
+									<ul class="submenu">
+										<li><a href="service.html">service One</a></li>
+										<li><a href="service2.html">service Two</a></li>
+										<li><a href="service3.html">service Single</a></li>
+									</ul>
+								</li>
+								<li><a href="worker.html">STYLERS <i class="fa fa-angle-down"></i></a>
+									<ul class="submenu">
+										<li><a href="worker.html">Stylers One</a></li>
+										<li><a href="worker2.html">Stylers Two</a></li>
+										<li><a href="worker3.html">Stylers Three <span class="pull-right">+</span></a>
+											<ul class="submenu">
+												<li><a href="#">Sabmenu One</a></li>
+												<li><a href="#">Sabmenu Two</a></li>
+												<li><a href="#">Sabmenu Three <span class="pull-right">+</span></a>
+                                                    <ul class="submenu">
+                                                        <li><a href="#">Sabmenu One</a></li>
+                                                        <li><a href="#">Sabmenu Two</a></li>
+                                                        <li><a href="#">Sabmenu Three</a></li>
+                                                        <li><a href="#">Sabmenu Four</a></li>
+                                                        <li><a href="#">Sabmenu Five</a></li>
+                                                    </ul>
+                                                </li>
+												<li><a href="#">Sabmenu Four</a></li>
+												<li><a href="#">Sabmenu Five</a></li>
+											</ul>
+										</li>
+										<li><a href="worker-detsils.html">Stylers Details</a></li>
+									</ul>
+								</li>
+								<li><a href="shop.html">Shop <i class="fa fa-angle-down"></i></a>
+                                    <ul class="submenu">
+										<li><a href="shop.html">Shop Page</a></li>
+										<li><a href="shop-details.html">Shop Single</a></li>
+										<li><a href="cart.html">Shopping Cart</a></li>
+										<li><a href="wishlist.html">Wishlist</a></li>
+										<li><a href="checkout.html">checkout</a></li>
+                                    </ul>
+                                </li>
+								<li><a href="blog.html">Blog <i class="fa fa-angle-down"></i></a>
+									<ul class="submenu right">
+										<li><a href="blog.html">Blog Grid  <span class="pull-right">+</span></a>
+											<ul class="submenu">
+												<li><a href="blog-grid-left-sidebar.html">Blog Left sidebar</a></li>
+												<li><a href="blog-grid-right-sidebar.html">Blog right sidebar</a></li>
+											</ul>
+										</li>
+										<li><a href="blog-list.html">Blog list  <span class="pull-right">+</span></a>
+											<ul class="submenu">
+												<li><a href="blog-list-left-sidebar.html">Blog Left sidebar</a></li>
+												<li><a href="blog-list-right-sidebar.html">Blog right sidebar</a></li>
+											</ul>
+										</li>
+										<li><a href="blog-details.html">Blog Details <span class="pull-right">+</span></a>
+                                            <ul class="submenu">
+												<li><a href="blog-details-left-sidebar.html">Blog Detsils Left sidebar</a></li>
+												<li><a href="blog-details-right-sidebar.html">Blog Detsils Right sidebar</a></li>
+											</ul>
+                                        </li>
+										<li><a href="blog-post.html">Blog Post <span class="pull-right">+</span></a>
+                                            <ul class="submenu">
+												<li><a href="blog-post-left-sidebar.html">Blog Post Left sidebar</a></li>
+												<li><a href="blog-post-right-sidebar.html">Blog Post Right sidebar</a></li>
+											</ul>
+                                        </li>
+									</ul>
+								</li>
+								<li><a href="contact.html">Contact</a></li>
+								<li><a href="#">Pages <i class="fa fa-angle-down"></i></a>
+									<ul class="megamenu">
+										<li>
+                                            <a class="mega-title" href="#">MegaMenu Title</a>
+											<ul>
+												<li><a href="about.html">About One</a></li>
+												<li><a href="about2.html">About Two</a></li>
+												<li><a href="service.html">service One</a></li>
+                                                <li><a href="service2.html">service Two</a></li>
+                                                <li><a href="service3.html">service Single</a></li>
+											</ul>
+										</li>
+										<li>
+                                            <a class="mega-title"  href="#">MegaMenu Title</a>
+											<ul>
+												<li><a href="shop.html">Shop Page</a></li>
+												<li><a href="shop-details.html">Shop Single</a></li>
+												<li><a href="wishlist.html">Wishlist</a></li>
+												<li><a href="cart.html">Shopping Cart</a></li>
+                                                <li><a href="checkout.html">checkout</a></li>
+											</ul>
+										</li>
+										<li>    
+                                            <a class="mega-title"  href="#">MegaMenu Title</a>
+											<ul>
+												<li><a href="portfolio.html">Portfolio</a></li>
+												<li><a href="portfolio2.html">Portfolio Two</a></li>
+												<li><a href="portfolio3.html">Portfolio Three</a></li>
+												<li><a href="portfolio4.html">Portfolio Mesonry</a></li>
+												<li><a href="portfolio5.html">Portfolio Mesonry Two</a></li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-1 col-sm-3 col-xs-3">
+						<div class="cart-wrap text-right">
+							<ul>
+								<li>
+									<a href="shop.html">
+										<span class="flaticon-shopping-cart-black-shape"></span>
+									</a>
+								</li>
+								<li>
+									<a href="javascript:void(0);">
+										<span class="flaticon-search"></span>
+									</a>
+									<ul>
+										<li>
+											<form action="#">
+												<input type="text" placeholder="Search here ..."/>
+												<button><span class="flaticon-search"></span></button>
+											</form>
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-1 clear col-xs-2 hidden-md hidden-lg">
+						<div class="responsive-menu-wrap floatright"></div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- heared area end -->
+		
